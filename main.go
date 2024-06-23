667bb49f9437fc2334d63e1a6c1c1ea21a0598b1
zachbuchli <zachbuchli@proton.me>
2024-06-23 11:24:42 -0700

    url paths worked out

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 )
 
 //go:embed templates
-var files embed.FS
+var templateDir embed.FS
 
 //go:embed static
-var statics embed.FS
+var staticsDir embed.FS
+
+const rootPath = "./files"
 
 var templates = map[string]*template.Template{
-	"index":   template.Must(template.ParseFS(files, "templates/layout.html", "templates/index.html")),
-	"about":   template.Must(template.ParseFS(files, "templates/layout.html", "templates/about.html")),
-	"message": template.Must(template.ParseFS(files, "templates/layout.html", "templates/message.html")),
-	"clicked": template.Must(template.ParseFS(files, "templates/clicked.html")),
+	"index": template.Must(template.ParseFS(templateDir, "templates/layout.html", "templates/index.html")),
+	"fb":    template.Must(template.ParseFS(templateDir, "templates/layout.html", "templates/fp.html")),
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,23 +28,9 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func clickedHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates["clicked"].Execute(w, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func aboutHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates["about"].Execute(w, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func msgHandler(w http.ResponseWriter, r *http.Request) {
-	msg := r.PathValue("msg")
-	err := templates["message"].Execute(w, msg)
+func filePathHandler(w http.ResponseWriter, r *http.Request) {
+	currPath := r.PathValue("path")
+	err := templates["fb"].Execute(w, currPath)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -52,12 +38,10 @@ func msgHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	http.Handle("/static/", http.FileServer(http.FS(statics)))
+	http.Handle("/static/", http.FileServer(http.FS(staticsDir)))
 
 	http.HandleFunc("/", indexHandler)
-	http.HandleFunc("/clicked", clickedHandler)
-	http.HandleFunc("/about", aboutHandler)
-	http.HandleFunc("/message/{msg}", msgHandler)
+	http.HandleFunc("/fb/{path...}", filePathHandler)
 
 	fmt.Println("starting server on port 8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
