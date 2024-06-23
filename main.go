02cb831486839ecf96d048a2a92f16b834857182
zachbuchli <zachbuchli@proton.me>
2024-06-23 16:33:55 -0700

    adding boostrap css

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 //go:embed templates
@@ -14,11 +16,16 @@ var templateDir embed.FS
 //go:embed static
 var staticsDir embed.FS
 
-const rootPath = "./files"
+const rootPath = "files"
+
+type FbFile struct {
+	Name string
+	Path string
+}
 
 type Fb struct {
 	CurrentPath string
-	SubPaths    []string
+	SubPaths    []FbFile
 	IsDirectory bool
 }
 
@@ -31,17 +38,45 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	err := templates["index"].Execute(w, nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
 func filePathHandler(w http.ResponseWriter, r *http.Request) {
 	currPath := r.PathValue("path")
-
+	fullPath := filepath.Join(rootPath, currPath)
+	fileInfo, err := os.Stat(fullPath)
+	if err != nil {
+		// going to assume this means file not found.
+		http.Error(w, "file not found", http.StatusNotFound)
+		return
+	}
 	fb := Fb{CurrentPath: currPath}
+	if fileInfo.IsDir() {
+		fb.IsDirectory = true
+		dir, err := os.Open(fullPath)
+		defer dir.Close()
+		if err != nil {
+			http.Error(w, "error opening dir", http.StatusInternalServerError)
+			return
+		}
+		files, err := dir.Readdir(-1)
+		if err != nil {
+			http.Error(w, "error reading dir files", http.StatusInternalServerError)
+			return
+		}
+		subs := make([]FbFile, len(files))
+		for i, f := range files {
+			subs[i].Path = currPath + "/" + f.Name()
+			subs[i].Name = f.Name()
+		}
+		fb.SubPaths = subs
+	}
 
-	err := templates["fb"].Execute(w, fb)
+	err = templates["fb"].Execute(w, fb)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
 }
 
